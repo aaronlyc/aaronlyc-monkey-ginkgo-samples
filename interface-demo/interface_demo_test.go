68abb5a14637900d3dce9a6b5e871c7ca1d31821
liuyuchao <blockchain_lyc@163.com>
2021-07-27 07:36:48 +0800

    add: 添加interface demo test

--- interface-demo/interface_demo_test.go
+++ interface-demo/interface_demo_test.go
@@ -1,8 +1,8 @@
 package interface_demo_test
 
 import (
-	interface_demo "aaronlyc/monkey-ginkgo-samples/interface-demo"
-	. "github.com/aaronlyc/monkey"
+	interfacedemo "aaronlyc/monkey-ginkgo-samples/interface-demo"
+	"github.com/aaronlyc/monkey"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"reflect"
@@ -11,16 +11,16 @@ import (
 var _ = Describe("InterfaceDemo", func() {
 	Describe("测试单个mock", func() {
 		It("当为etcd时", func() {
-			e := &interface_demo.Etcd{}
-			dbPatch := Patch(interface_demo.NewDb, func(_ string) interface_demo.Db {
+			e := &interfacedemo.Etcd{}
+			dbPatch := monkey.Patch(interfacedemo.NewDb, func(_ string) interfacedemo.Db {
 				return e
 			})
 			defer dbPatch.Unpatch()
 
-			db := interface_demo.NewDb("mysql")
+			db := interfacedemo.NewDb("mysql")
 			info := "hello interface"
-			patches := PatchInstanceMethod(reflect.TypeOf(e), "Retrieve",
-				func(_ *interface_demo.Etcd, _ string) (string, error) {
+			patches := monkey.PatchInstanceMethod(reflect.TypeOf(e), "Retrieve",
+				func(_ *interfacedemo.Etcd, _ string) (string, error) {
 					return info, nil
 				})
 			defer patches.Unpatch()
